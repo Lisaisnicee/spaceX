ac44d9035535f7b686877aef698423951b2a9611
mac <mac@MacBook-Air-de-mac.local>
2023-07-11 16:40:45 +0200

    first commit

---
+++ src/component/searchC.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function SearchC() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+
+  const fetchData = async () => {
+    const url = 'https://api.spacexdata.com/v4/crew';
+    try {
+      
+      const response = await axios.get(url);
+      console.log('data', response)
+      setData({name: response.data.name,
+        agency :response.data.agency ,
+        image : response.data.image,
+        wikipedia :response.data.wikipedia ,
+        launches :response.data.launches[0],
+        status : response.data.status ,
+        id :response.data.id ,
+    })
+      
+      
+      setError(null);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData('');
+  }, []);
+
+  
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+    
+    <div>
+       {/* Afficher les données */}
+       {data && (
+           <ul>
+               {data.map((item) => (
+                   <li key={item.id}>{item.name}</li>
+               ))}
+           </ul>
+       )}
+   </div>
+    
+
+    </>
+  );
+}
+
+export default SearchC;
--- src/pages/viewCrew.js
+++ src/pages/viewCrew.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import SearchC from '../component/searchC';
 
 const ViewCrew = () => {
   return (
     <>
       <div>
        <h1>viewCrew here</h1>
+       <SearchC />
       </div>
     </>
   );
